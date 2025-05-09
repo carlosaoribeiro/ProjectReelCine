1980d936f13f60ad123195145aae669cd3a924f3
carlosribeiro <carlos.a.o.r@icloud.com>
2025-05-09 18:47:17 -0500

    Logica Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services") // Plugin para integração com Firebase
 }
 
 android {
@@ -26,23 +27,30 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
+    // Bibliotecas AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Firebase Authentication (auth)
     implementation(libs.firebase.auth.ktx)
+    implementation(platform(libs.firebase.bom)) // Para integrar o Firebase BOM
+
+    // Testes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
