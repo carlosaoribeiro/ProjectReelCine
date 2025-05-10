f6a72032bdae21a64cbfeca08054325f35a040ca
carlosribeiro <carlos.a.o.r@icloud.com>
2025-05-10 08:34:11 -0500

    Integracao API TMDB

--- app/src/main/java/com/carlosribeiro/reelcineproject/api/TMDBService.kt
+++ app/src/main/java/com/carlosribeiro/reelcineproject/api/TMDBService.kt
@@ -1,4 +1,15 @@
 package com.carlosribeiro.reelcineproject.api
 
-class TMDBService {
+import com.carlosribeiro.reelcineproject.model.FilmeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "pt-BR"
+    ): Call<FilmeResponse>
 }
