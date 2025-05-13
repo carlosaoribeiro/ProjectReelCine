140bbd2611ef8456511ad99cd0aef6b035953f7a
carlosribeiro <carlos.a.o.r@icloud.com>
2025-05-13 11:13:33 -0500

    cadastro-viewbinding

--- app/src/main/java/com/carlosribeiro/reelcineproject/api/RetrofitInstance.kt
+++ app/src/main/java/com/carlosribeiro/reelcineproject/api/RetrofitInstance.kt
@@ -4,9 +4,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
--- app/src/main/java/com/carlosribeiro/reelcineproject/api/TMDBService.kt
+++ app/src/main/java/com/carlosribeiro/reelcineproject/api/TMDBService.kt
@@ -1,25 +1,40 @@
 package com.carlosribeiro.reelcineproject.api
 
-import com.carlosribeiro.reelcineproject.BuildConfig
 import com.carlosribeiro.reelcineproject.model.FilmeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import com.carlosribeiro.reelcineproject.BuildConfig
 
 interface TMDBService {
 
-    @GET("movie/popular")
-    suspend fun getPopularMovies(
-        @Query("api_key") apiKey: String = "8fdc23c3632ca01c557a2aec34674d9f",
-        @Query("language") language: String = "pt-BR",
-        @Query("page") page: Int = 1
+    @GET("trending/movie/day")
+    suspend fun buscarFilmesEmAlta(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY
     ): Response<FilmeResponse>
 
-    // 🔍 Busca por nome (para RecomendarFilmeActivity)
+    @GET("movie/now_playing")
+    suspend fun buscarFilmesRecentes(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
+        @Query("language") language: String = "pt-BR"
+    ): Response<FilmeResponse>
+
+    @GET("movie/upcoming")
+    suspend fun buscarFilmesLancamentos(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
+        @Query("language") language: String = "pt-BR"
+    ): Response<FilmeResponse>
+
+    @GET("movie/top_rated")
+    suspend fun buscarFilmesTopAvaliados(
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
+        @Query("language") language: String = "pt-BR"
+    ): Response<FilmeResponse>
+
     @GET("search/movie")
     suspend fun buscarFilmes(
         @Query("query") query: String,
-        @Query("api_key") apiKey: String = "8fdc23c3632ca01c557a2aec34674d9f",
+        @Query("api_key") apiKey: String = BuildConfig.TMDB_API_KEY,
         @Query("language") language: String = "pt-BR"
     ): Response<FilmeResponse>
 }
